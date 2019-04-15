6fb54bbb70e09723c1dae4ded5a462e61679e3c5
SOIP <38543876+sumofitsparts@users.noreply.github.com>
2019-04-15 00:14:19 -0400

    added clock in, clock out, and reset buttons

--- public/script.js
+++ public/script.js
@@ -11,15 +11,82 @@ $(document).ready(function() {
             dataType: "json",
            }).done(function( data ) {
                 if (data == "") {
-                    $('#result').html('');
+                    $('#result').html("<button id='clockin' class='btn btn-success'>Clock In</Button>");
                 } else {
+                    console.log("length: " + data.length);
                     $( "#result" ).append("<table id='resultTable' class='table'><tr><th>Date</th><th>Start Time</th><th>End Time</th></tr>");
-                    for (var i = 0; i < 14; i++) {
+                    for (var i = 0; i < data.length; i++) {
                      $( "#resultTable" ).append("<tr><td>" + data[i].date + "</td><td>" + data[i].starthh + ":" + data[i].startmm + "</td><td>" + data[i].endhh + ":" + data[i].endmm + "</td></tr>");
                     }
-                    $( "#resultTable" ).append("</table>");
+                    console.log("end hour: " + data[data.length-1].endhh);
+                    if (data[data.length-1].endhh != "") {
+                        $( "#resultTable" ).append("</table><br><button id='clockin' class='btn btn-success'>Clock In</Button><button id='delete' class='btn btn-danger float-right'>Reset</Button>");
+                    } else {
+                        $( "#resultTable" ).append("</table><br><button id='clockout' class='btn btn-warning'>Clock Out</Button><button id='delete' class='btn btn-danger float-right'>Reset</Button>");
+                    }
                 }
             console.log("data returned")
+            $('#clockin').on("click", function() {
+                var id = data.length+1;
+                $.ajax({
+                    method: "POST",
+                    url: '/updatein/' + id + '/' + text,
+                    dataType: "json",
+                    success: function(datas, textStatus, jqXHR)
+                    {
+                        console.log("clocked in")
+                       $('#employeeFormBtn').trigger('click');
+                       alert("You have clocked in")
+                    
+                    },
+                    error: function (jqXHR, textStatus, errorThrown)
+                    {
+                        alert("something went wrong.  Unable to clock in")
+                    }
+                   }).then(function( datas ) {
+                       
+                });
+            });
+            $('#clockout').on("click", function() {
+                var id = data.length;
+                $.ajax({
+                    method: "POST",
+                    url: '/updateout/' + id + '/' + text,
+                    dataType: "json",
+                    success: function(datas, textStatus, jqXHR)
+                    {
+                        console.log("clocked out")
+                       $('#employeeFormBtn').trigger('click');
+                       alert("You have clocked out")
+                    },
+                    error: function (jqXHR, textStatus, errorThrown)
+                    {
+                        alert("something went wrong.  Unable to clock out")
+                    }
+                   }).then(function( datas ) {
+                       
+                });
+            });
+            $('#delete').on("click", function() {
+                $.ajax({
+                    method: "POST",
+                    url: '/delete/' + text,
+                    dataType: "json",
+                    success: function(datas, textStatus, jqXHR)
+                    {
+                        console.log("clocked out")
+                       $('#employeeFormBtn').trigger('click');
+                       alert("All times Erased")
+                    },
+                    error: function (jqXHR, textStatus, errorThrown)
+                    {
+                        alert("something went wrong.  Unable to clock out")
+                    }
+                   });
+            });
         });
     });
+
+    
+
 });  // end document ready
--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.get('/employee/:id', function(req, res){
     var employeeNo = req.params.id;
 
     MongoClient.connect(process.env.MONGODB_URI2, { useNewUrlParser: true }, function(err, db) {
+    // MongoClient.connect("mongodb://localhost:27017/", { useNewUrlParser: true }, function(err, db) {
         if (err) throw err;
         var dbo = db.db("heroku_lsmczchn");
         // var dbo = db.db("mydb");
@@ -24,6 +25,85 @@ app.get('/employee/:id', function(req, res){
           db.close();
         });
       });
+});
+
+app.post('/updatein/:id/:text', function(req, res){
+  console.log(req.params.id + req.params.text);
+  var timeID = req.params.id;
+  var employee = req.params.text;
+MongoClient.connect(process.env.MONGODB_URI2, function(err, db) {
+// MongoClient.connect("mongodb://localhost:27017/", function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("heroku_lsmczchn");
+  // var dbo = db.db("mydb");
+    var currentDate = new Date();
+    var month = currentDate.getMonth() + 1
+    var day = currentDate.getDate()
+    var year = currentDate.getFullYear()
+    var date = month + "/" + day + "/" + year;
+    console.log(date);
+    var currentHour = new Date();
+    var hour = currentHour.getHours();
+    console.log(hour);
+    var currentMin = new Date();
+    var min = currentMin.getMinutes();
+    console.log(min);
+  var myobj = {  _id: timeID, date: date, starthh: hour, startmm: min, endhh: '', endmm: ''};
+  dbo.collection(employee).insertOne(myobj, function(err, result) {
+    if (err) throw err;
+    res.json(result);
+    console.log("1 document inserted");
+    db.close();
+  });
+});
+});
+
+app.post('/updateout/:id/:text', function(req, res){
+  console.log(req.params.id + req.params.text);
+  var timeID = req.params.id;
+  var employee = req.params.text;
+MongoClient.connect(process.env.MONGODB_URI2, function(err, db) {
+// MongoClient.connect("mongodb://localhost:27017/", function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("heroku_lsmczchn");
+  // var dbo = db.db("mydb");
+    var currentDate = new Date();
+    var month = currentDate.getMonth() + 1
+    var day = currentDate.getDate()
+    var year = currentDate.getFullYear()
+    var date = month + "/" + day + "/" + year;
+    console.log(date);
+    var currentHour = new Date();
+    var hour = currentHour.getHours();
+    console.log(hour);
+    var currentMin = new Date();
+    var min = currentMin.getMinutes();
+    console.log(min);
+  var myobj = {  _id: timeID };
+  var endtime = {$set: {endhh: hour, endmm: min} }
+  dbo.collection(employee).updateOne(myobj, endtime, function(err, result) {
+    if (err) throw err;
+    res.json(result);
+    console.log("1 document updated");
+    db.close();
+  });
+});
+});
+
+app.post('/delete/:id', function(req, res){
+MongoClient.connect(process.env.MONGODB_URI2, function(err, db) {
+// MongoClient.connect("mongodb://localhost:27017/mydb", function(err, db) {
+  // var employeeNo = req.params.id;
+  if (err) throw err;
+  var dbo = db.db("heroku_lsmczchn");
+  var dbo = db.db("mydb");
+  dbo.dropCollection(employeeNo, function(err, delOK) {
+    if (err) throw err;
+    if (delOK) console.log("employee1 Collection deleted");
+    res.json(delOK);
+    db.close();
+  });
+});
 });
 
 app.listen(port, () => console.log(`Time Sheet app listening on port ${port}!`));
